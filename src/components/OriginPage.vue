4cd0feb607b0f906a0541d10319ea13fb9642857
uxbecks <uxbecks@gmail.com>
2018-04-22 19:23:51 -0700

    Update OriginPage.vue

--- src/components/OriginPage.vue
+++ src/components/OriginPage.vue
@@ -6,28 +6,26 @@
         <div class="origin-info" id="mountain">
 
             <div class="columns">
-                <div class="column.is-3">
+                <div class="column is-4">
                 </div>
 
-                <div class="column.is-9">
+                <div class="column is-8">
                     <h3>RockClaw Meowntain</h3>
                     <h4>A Kitty that comes from a craggy mountainous area.</h4>
-                    <p>Origin Identity: Strong cats that are known for their individual
-                        power, but lack the ability to work well with others. Always
-                        remember that one is a lonely number.</p>
+                    <p>Origin Identity: Strong cats that are known for their individual power, but lack the ability to work well with others. Always remember that one is a lonely number.</p>
                     <p>Strong against Farms  /  Weak against Forest</p>
                 </div>
             </div>
 
             <div class="columns">
-                <div class="column.is-3">
+                <div class="column is-4">
                     <ul>Mountain Cat Types:</ul>
                     <ul>Himalayan</ul>
                     <ul>Chartreux</ul>
                     <ul>Mainecoon</ul>
                 </div>
 
-                <div class="column.is-9">
+                <div class="column is-8">
                     <img src="assets/himalayan%20mountain.svg" class="image.is-200x200" alt="Mountain Cat 1" >
                     <img src="" class="image.is-200x200" alt="Mountain Cat 2" >
                     <img src="assets/mainecoon%20mountains.svg" class="image.is-200x200" alt="Mountain Cat 3" >
@@ -35,6 +33,184 @@
             </div>
 
         </div>
+        
+        
+        <div class="origin-info" id="desert">
+
+            <div class="columns">
+                <div class="column is-4">
+                </div>
+
+                <div class="column is-8">
+                    <h3>The Savanna</h3>
+                    <h4>A Kitty that is at home on the sand dunes of a Desert, but wouldn’t want to find themselves without a coat in the Arctic!</h4>
+                    <p>Origin Identity: Lethal cats known for their bursty speed and ability to jump while attacking with incredible accuracy, but make sure to look before you leap. </p>
+                    <p>Strong against Arctic  /  Weak against Jungle </p>
+                </div>
+            </div>
+
+            <div class="columns">
+                <div class="column is-4">
+                    <ul>Desert Cat Types:</ul>
+                    <ul>Sphynx</ul>
+                    <ul>Munchkin</ul>
+                    <ul>Laperm</ul>
+                </div>
+
+                <div class="column is-8">
+                    <img src="" class="image.is-200x200" alt="Desert Cat 1" >
+                    <img src="" class="image.is-200x200" alt="Desert Cat 2" >
+                    <img src="" class="image.is-200x200" alt="Desert Cat 3" >
+                </div>
+            </div>
+
+        </div>
+        
+        
+        
+         <div class="origin-info" id="arctic">
+
+            <div class="columns">
+                <div class="column is-4">
+                </div>
+
+                <div class="column is-8">
+                    <h3>Pawlaska</h3>
+                    <h4>A Kitty that comes from the cold Arctic and will avoid the Desert at all costs!</h4>
+                    <p>Origin Identity: One of the most persistent hunters in the cat family, they are known to slow their enemies before executing a lethal blow. Be wary when choosing your prey, as it can be your melting point. </p>
+                    <p>Strong against Seaside  /  Weak against Desert </p>
+                </div>
+            </div>
+
+            <div class="columns">
+                <div class="column is-4">
+                    <ul>Arctic Cat Types:</ul>
+                    <ul>Pixebob</ul>
+               
+                </div>
+
+                <div class="column is-8">
+                    <img src="" class="image.is-200x200" alt="Arctic Cat 1" >
+                </div>
+            </div>
+
+        </div>
+        
+         
+         <div class="origin-info" id="seaside">
+
+            <div class="columns">
+                <div class="column is-4">
+                </div>
+
+                <div class="column is-8">
+                    <h3>Purrsian Gulf</h3>
+                    <h4>A Kitty that comes from a Seaside biome but despises the cold.</h4>
+                    <p>Origin Identity: Creative by nature, these cats tend to change the tide of the battle by boosting up those around them. Always remember to help yourself first, before helping others.</p>
+                    <p>Strong against City  /  Weak against Arctic </p>
+                </div>
+            </div>
+
+            <div class="columns">
+                <div class="column is-4">
+                    <ul>Seaside Cat Types:</ul>
+                    <ul>Bobtail</ul>
+               
+                </div>
+
+                <div class="column is-8">
+                    <img src="" class="image.is-200x200" alt="Seaside Cat 1" >
+                </div>
+            </div>
+
+        </div>
+        
+        
+        <div class="origin-info" id="city">
+
+            <div class="columns">
+                <div class="column is-4">
+                </div>
+
+                <div class="column is-8">
+                    <h3>Mew York City</h3>
+                    <h4>A Kitty that knows its way around the City, but can get washed up if strays too far from home. </h4>
+                    <p>Origin Identity: Strong cats that can hold their ground, where they lack in speed, they make up in power! These cats must always remember to be one step ahead, one wrong move and they will find themselves in trouble.</p>
+                    <p>Strong against Forest  /  Weak against Seaside </p>
+                </div>
+            </div>
+
+            <div class="columns">
+                <div class="column is-4">
+                    <ul>City Cat Types:</ul>
+                    <ul>Ragamuffin</ul>
+                    <ul>Ragdoll</ul>
+                    <ul>Persian</ul>
+                </div>
+
+                <div class="column is-8">
+                    <img src="" class="image.is-200x200" alt="City Cat 1" >
+                    <img src="" class="image.is-200x200" alt="City Cat 2" >
+                    <img src="" class="image.is-200x200" alt="City Cat 3" >
+                </div>
+            </div>
+
+        </div>
+          
+          
+        <div class="origin-info" id="plains">
+
+            <div class="columns">
+                <div class="column is-4">
+                </div>
+
+                <div class="column is-8">
+                    <h3>Kittybrook Farms</h3>
+                    <h4>A Kitty that comes from a home on the range.</h4>
+                    <p>Origin Identity: Small but lethal kitties, known to attack at mind blowing speeds! Make sure to pick on someone your own size, unless your name is David.</p>
+                    <p>Strong against Jungle  /  Weak against Mountain </p>
+                </div>
+            </div>
+
+            <div class="columns">
+                <div class="column is-4">
+                    <ul>Plains Cat Types:</ul>
+                    <ul>Cymric</ul>
+                </div>
+
+                <div class="column is-8">
+                    <img src="" class="image.is-200x200" alt="Plains Cat 1" >
+                </div>
+            </div>
+        </div>
+ 
+        <div class="origin-info" id="jungle">
+
+            <div class="columns">
+                <div class="column is-4">
+                </div>
+
+                <div class="column is-8">
+                    <h3>Jungles of Papua Mew Guinea</h3>
+                    <h4>A Kitty that live in the high trees of the jungles.</h4>
+                    <p>Origin Identity: Poisonous kitties that damage and weaken their enemies when bitten.  They can really put a hitch to your leap, so best be getting them before they get you.</p>
+                    <p>Strong against Desert  /  Weak against  </p>
+                </div>
+            </div>
+
+            <div class="columns">
+                <div class="column is-4">
+                    <ul>Jungle Cat Types:</ul>
+                    <ul>Birman</ul>
+                </div>
+
+                <div class="column is-8">
+                    <img src="" class="image.is-200x200" alt="Plains Cat 1" >
+                </div>
+            </div>
+        </div>
+ 
+ 
     </div>
 </template>
 
