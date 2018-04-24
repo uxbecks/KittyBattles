68c76e77716f0b5275555fde389e510c81745ba2
uxbecks <uxbecks@gmail.com>
2018-04-23 22:52:14 -0700

    Update AboutUsPage.vue

--- src/components/AboutUsPage.vue
+++ src/components/AboutUsPage.vue
@@ -1,6 +1,140 @@
 <template>
     <div class="container">
-        <h1 class="title">About Us</h1>
+    <h1>Meet the KittyBattles Team</h1>
+    
+        <div class="personal-info">
+           <h3 class="column is-12 job-title">Project Lead / Backend Developer</h3>
+             
+           <div class="column is-12">
+              <img src="public/assets/" class="profile-image is-200x200">
+           </div>
+           
+           <h4 class="column is-12 real-name">Glenn Eggleton</h4>
+            
+           <h5 class="column is-12 media-name">(aka geggleto)</h5>
+           
+           <p class="column is-12 100bio">A little about me: Strong cats that are known for their individual power, but lack </br>
+           the ability to work well with others. Always remember that one is a lonely number.</br>
+           There should be room for another sentence of text here to make the bio long enough.</br>
+           There should be room for another sentence of text here to make the bio long enough.</p>
+           
+           <p class="column is-12">What's your favorite CryptoKitten?</p>
+           
+           <p class="column is-12">What's your favorite CryptoKitties trait?</p>
+           
+           <p class="column is-12">What's your favorite KittyBattles origin?</p>
+           
+           <p class="column is-12">What's your favorite KittyBattles skill?</p>
+
+        </div>
+        
+        
+        
+        <div class="personal-info">
+           <h3 class="column is-12 job-title">Operations Manager</h3>
+             
+           <div class="column is-12">
+              <img src="public/assets/" class="profile-image is-200x200">
+           </div>
+           
+           <h4 class="column is-12 real-name">Jim McNelis</h4>
+            
+           <h5 class="column is-12 media-name">(aka kitty.fyi)</h5>
+           
+           <p class="column is-12">CryptoKitty most like me: </p>
+           
+           <p class="column is-12 100bio"></p>
+           
+           <p class="column is-12">What's your favorite CryptoKitten? </p>
+           
+           <p class="column is-12">What's your favorite CryptoKitties trait? </p>
+           
+           <p class="column is-12">What's your favorite KittyBattles origin?  </p>
+           
+           <p class="column is-12">What's your favorite KittyBattles skill? </p>
+
+        </div>
+        
+        
+        
+        <div class="personal-info">
+           <h3 class="column is-12 job-title">Lead Game Designer</h3>
+             
+           <div class="column is-12">
+              <img src="public/assets/" class="profile-image is-200x200">
+           </div>
+           
+           <h4 class="column is-12 real-name">Steven Miyauchi</h4>
+            
+           <h5 class="column is-12 media-name">(aka WittyKitty)</h5>
+           
+           <p class="column is-12">CryptoKitty most like me: Vernon; because the unconventional artwork defines my play and work style of trying to overcome the most difficult challenges.  The split image of the Vernon illustrates my work ethics of staying up day and night until the job is complete while maintaining a colorful image.</p>
+           
+           <p class="column is-12 100bio"> I am 25 years of age, full Japanese and fourth generation in America.  I attended the University of Hawaii for two years only to move back from my original home of Seattle to attend the University of Washington.  I dropped College to pursue my career in the tech/gaming industry and am here now.  I am really excited about this opportunity to add more usability to  our precious CryptoKitties for this amazing community!</p>
+           
+           <p class="column is-12">What's your favorite CryptoKitten? My favorite CK is the <a href="https://www.cryptokitties.co/kitty/690404">beautiful angel cat</a>.  As angelic as the appearance, the wasntme face adds a mysterious and up to no good factor to the mix.</p>
+           
+           <p class="column is-12">What's your favorite CryptoKitties trait? My favorite CK trait has to be Daemonwings because aside from looking amazing, it opens up the possibility to CK evolving into a game with flying creatures which is amazing in my eyes.</p>
+           
+           <p class="column is-12">What's your favorite KittyBattles origin? My favorite origin has to be seaside because it reminds me of a water type creature which have always been my favorite because they usually seem to be the most versatile from my experience in battle games. </p>
+           
+           <p class="column is-12">What's your favorite KittyBattles skill? My favorite KB skill is sandstorm because if used in the right scenario, will have my favorite animation added as an Easter egg.</p>
+
+        </div>
+        
+        
+        <div class="personal-info">
+           <h3 class="column is-12 job-title">Lead Designer</h3>
+             
+           <div class="column is-12">
+              <img src="public/assets/" class="profile-image is-200x200">
+           </div>
+           
+           <h4 class="column is-12 real-name">Bx Dawes</h4>
+            
+           <h5 class="column is-12 media-name">(aka BxKat)</h5>
+           
+           <p class="column is-12">CryptoKitty most like me: </p>
+           
+           <p class="column is-12 100bio"></p>
+           
+           <p class="column is-12">What's your favorite CryptoKitten? </p>
+           
+           <p class="column is-12">What's your favorite CryptoKitties trait? </p>
+           
+           <p class="column is-12">What's your favorite KittyBattles origin?  </p>
+           
+           <p class="column is-12">What's your favorite KittyBattles skill? </p>
+
+        </div>
+        
+        
+        <div class="personal-info">
+           <h3 class="column is-12 job-title">Front-End Developement</h3>
+             
+           <div class="column is-12">
+              <img src="public/assets/" class="profile-image is-200x200">
+           </div>
+           
+           <h4 class="column is-12 real-name">Candi Johnson</h4>
+            
+           <h5 class="column is-12 media-name">(aka rudemOOse)</h5>
+           
+           <p class="column is-12">CryptoKitty most like me: </p>
+           
+           <p class="column is-12 100bio"></p>
+           
+           <p class="column is-12">What's your favorite CryptoKitten? </p>
+           
+           <p class="column is-12">What's your favorite CryptoKitties trait? </p>
+           
+           <p class="column is-12">What's your favorite KittyBattles origin?  </p>
+           
+           <p class="column is-12">What's your favorite KittyBattles skill? </p>
+
+        </div>
+        
+        
     </div>
 </template>
 
