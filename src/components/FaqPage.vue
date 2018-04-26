6395b9d86dfbfecef7c0ddaeed9c94466c8f4176
uxbecks <uxbecks@gmail.com>
2018-04-25 22:07:55 -0700

    Update FaqPage.vue

--- src/components/FaqPage.vue
+++ src/components/FaqPage.vue
@@ -1,40 +1,147 @@
 <template>
     <div class="container">
-        <h1 class="title is-1">FAQS AND GLOSSARY</h1>
+      
+      <h1 class="title is-1">FAQS AND GLOSSARY</h1>
         <aside class="menu">
-            <p class="title is-2">
-                GETTING STARTED
+      
+      
+      <p class="title is-2">
+                KITTYBATTLES BASICS
             </p>
+      
+      
             <ul class="menu-list">
+            
                 <li>
                     <a>What is KittyBattles?</a>
+                    <ul>
+                        <li>
+                            <p>KittyBattles is a turn-based arena battle game where a team of 3 of your CryptoKitties battles against other teams of CryptoKitties. Pawsome!</p>
+                        </li>
+                    </ul>
                 </li>
+                
                 <li>
                     <a>Does it cost anything to play KittyBattles?</a>
+                    <ul>
+                        <li>
+                            <p>KittyBattles is Free-to-play, but you must own 3 <a href="https://www.cryptokitties.co/">CryptoKitties</a> and have <a href="https://metamask.io/">Metamask</a> installed (free) to create a team.</p>
+                        </li>
+                    </ul>
                 </li>
+                
                 <li>
                     <a>How do I login to KittyBattles?</a>
                     <ul>
                         <li>
-                            <p>Logging into KittyBattles is handled the same way you log into CryptoKitties, via Metamask.</p>
+                            <p>Logging into KittyBattles is handled the same way you log into CyptoKitties, via Metamask.  Once you sign the authentication request from Metamask, your will be able to assemble your first Battle Team.</p>
                         </li>
                     </ul>
                 </li>
+                
             </ul>
+            
+            
              <p class="title is-2">
-               BUILDING TEAMS
+               BUILDING YOUR TEAMS
             </p>
+            
+            
             <ul class="menu-list">
+                
                 <li>
-                    <a>What do I need to make a Battle Team?</a>
+                    <a>What are Battle Teams and how do I make one?</a>
+                    <ul>
+                        <li>
+                            <p>Battle Teams consist of 3 of your Kitties. You can have a maximum of 3 Battle Teams, but only need 1 team to play. Kitties can be added or retired from your Battle Teams. If you retire a Kitty from a Battle Team, you will not be able to add them to another team for 7 days.</p>
+                            <p>You will pick your team from your linked wallet.  You can have up to 3 Battle Teams, so you can fill your Team Roster with up to 9 Kitties.  Mix and match these kitties to maximize your team’s potential.</p>
+                        </li>
+                    </ul>
                 </li>
+                
+                
+               <li>
+                    <a>What happens when I "retire" a kitten?</a>
+                    <ul>
+                        <li>
+                            <p>When you “retire” a kitten, that kitten simply returns to your wallet for 7 days.  The kitty will retain all XP and SP acquired, so when they return to battle they will not have to start over.</p>
+                        </li>
+                    </ul>
+                </li>
+                
+                <li>
+                    <a>What happens to a kitten's XP/BP/SP when I sell it?</a>
+                    <ul>
+                        <li>
+                            <p>When you sell a Battle Kitten, that kitten will retain all XP and SP earned while in your care.  So battle those “shit cats” to make them the most sought after kitties in the Kittyverse.</p>
+                        </li>
+                    </ul>
+                </li>
+                
+           </ul>      
+            
+            
+            
+             <p class="title is-2">
+               GAMEPLAY
+            </p>
+            
+       <ul class="menu-list"> 
                 <li>
                     <a>What are Kitty Origins?</a>
+                    <ul>
+                        <li>
+                            <p>Kitty Origins are determined by the Fur catribute of your CryptoKitty and cannot be changed.  Every CyptoKitty fits within a Kitty Origin and each Kitty Origin has unique enhancements to their Battle Skills.</p>
+                            <p>There are 8 Kitty Origins: Arctic, Desert, Jungle, City, Mountain, Plains, Seaside, & Space. <a href="OriginPage.vue"> Click here to learn which kitty belongs to which origin</a>!</p>
+                        </li>
+                    </ul>
                 </li>
+           
                 <li>
-                    <a>What are Kitty Classes?</a>
+                    <a>How do my Kitties gain experience and level up?</a>
+                    <ul>
+                        <li>
+                            <p>As you battle, your Kitties will gain both Experience Points (XP) and Battle Points (BP). Once a Kitty earns enough XP, they will level up and earn a Skill Point. Skill Points can be used to level up acquired Battle Skills.  Battle Points are a form of currency that you can spend to play in the Battle Arena.</p>
+                        </li>
+                    </ul>
                 </li>
-            </ul>
+           
+           
+           
+          </ul>  
+           
+           
+           
+           <p class="title is-2">
+               Glossary of Terms
+            </p>
+            
+            
+            <ul class="menu-list">
+                
+                <li>
+                    <strong>Hit Points (HP) -</strong> aka. Health Points.  These points are removed when you take damage and restored when you are healed.
+                </li>
+                
+                <li>
+                    <strong>Experience Points (XP) -</strong> Points that each of your Kitties earn for completing and winning battles.  These will level up your kitties.
+                </li>
+                
+                <li>
+                    <strong>Skill Points (SP) -</strong> Points that are earned each time you level up that are used to better your learned skills.
+                </li>
+           
+                <li>
+                    <strong>Battle Points (HP) -</strong> Points earned in the Dojo (and Battle Arena) that can be redeemed to play in the Battle Arena.
+                </li>
+             </ul>
+             
+             
+             
+             
+             
+             
+           
         </aside>
     </div>
 </template>
