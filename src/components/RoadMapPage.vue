65bd7321302231161a47462eb749144fbe048f68
uxbecks <uxbecks@gmail.com>
2018-05-06 12:41:29 -0700

    Update RoadMapPage.vue

--- src/components/RoadMapPage.vue
+++ src/components/RoadMapPage.vue
@@ -1,6 +1,39 @@
 <template>
     <div class="container">
         <h1 class="title">Road Map</h1>
+        
+        v1.1 -   Initial Alpha Testing
+
+        A limited number of pawesome player will be invited to take place in the Alpha test. With their help, we will smooth over the rough edges so we can open the game up to a wider audience. This phase will focus primarily on the battles themselves.  Other parts of the game may not be functional at this time.  Player progress will be reset for the Beta.
+
+        public/assets/Butterflies_Right.svg
+        
+        public/assets/Butterflies_Left.svg
+
+        v1.2  -   Initial Beta Testing
+
+        A larger group of players will be invited to take place in the Beta test.  During this phase, more sections of the website will become functional so they can be debugged and improved according to feedback from the users.  The battle system will be improved as well, based on additional feedback from this phase. Player progress will be reset for the Final Beta
+
+
+        v1.3 -   Final Beta Testing
+
+        Alpha and Beta testers will have the opportunity to invite a few friends to try the game, along with any players who have expressed interest but have not been invited to a previous release.  During this phase, we will iron out all the little details that still have some wrinkles and look for final feedback on improvements before our public launch.  Player progress will be reset for the Public Launch.
+
+
+        public/assets/Butterflies_Right.svg
+        
+        public/assets/Butterflies_Left.svg
+        
+        
+        v1.4 -   Public Launch
+
+        Any user can sign up and play KittyBattles!  Players will be able to form their teams, battle other players, earn battle points, give their kitties additional skills, and purchase items from the shop.  Who will be the first to discover the powerful cattributes for each Origin?
+
+        v2.1 -   Tournaments Release
+
+        Who will reign over the Kittyverse as Champion of KittyBattles? When we release tournaments, that question will be answered.  Battle your way to the top of the ladder,  win the respect of your peers, and claim the glory all for yourself!  Tournaments will be a premium feature with a cost for entry. Unique prizes will be available to those who achieve glory.
+        
+        
     </div>
 </template>
 
